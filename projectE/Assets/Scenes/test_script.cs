3e83f2f4fa17860506451e82faa988942bc137e1
B877032_JJH <zero9936@gmail.com>
2022-03-13 20:26:58 +0900

    테스트 스크립트 생성
    깃헙 연동 성공 여부 확인

---
+++ projectE/Assets/Scenes/test_script.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_script : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //test
+    }
+}
